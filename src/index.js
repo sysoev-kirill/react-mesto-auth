3071ad548de0634f931f7701b6c46b6518bbfd2e
sysoev-kirill <sysoev.kirill@gmail.com>
2023-06-16 22:38:19 +0300

    React-auth complited

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-  <App />
-  </BrowserRouter>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
 
 
